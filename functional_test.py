eb5814f4ca117533d4dcf541f731383835647df4
Oh <uidg3913@contiwan.com>
2017-05-30 15:48:49 +0900

    기능 테스트가 작업 아이텔을 추가할 수 있는지 확인

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import unittest
 
 class NewCisitorTest(unittest.TestCase):
@@ -11,35 +13,38 @@ class NewCisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # chajin heard a new tasks web site came out
-        # and check the web site
+        # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
+        # 해당 웹 사이트를 확인하러 간다
         self.browser.get('http://localhost:8000/')
 
-        # web pages , titel and header has 'To-Do'
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        # 웹 페이지 타이틀과 헤더가 ‘To-Do’를 표시하고 있다
+        header_text = self.browser.find_element_by_tag_name('h1').text
 
-        #he decided to add a new task
+        # 그녀는 바로 작업을 추가하기로 한다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'))
 
-        #input "공작 기털 사기" in text box
-        #(his hobby is a net make for 날치 잡이용)
+        # "공작깃털 사기"라고 텍스트 상자에 입력한다
+        # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
+        inputbox.send_keys(Keys.ENTER)
 
-        # if key in Enter , reload a page ,
-        # "1 : 공장 깃털 사기" item will be added
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+            "신규 작업 아이템이 테이블에 표시되지 않는다"
+        )
 
-        # there are more text boxes for additional items' input
-        # input "공작 깃털을 이용해서 그물 만들기"
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
+        # 다시 "공작깃털을 사용해서 그물 만들기"라고 입력한다 (에디스는 매우 체계적인 사람이다)
+        self.fail("finish the test!")
 
-        # page reloaded , two items are in the list.
-        # he wants to check if the site stored the items.
-        # the site creats to show them with a new URL
-        # the URL's description shows.
-
-        # if connect to the URL , he can see the items
-
-        # he will be satisfied with it and sleep.
-
-        browser.quit()
+        # 페이지는 다시 갱신되고, 2개 아이템이 목록에 보여진다
+        # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다.
+        # 사이트는 그녀를 위한 특정 URL을 생성해 준다.
+        # 이때 URL에 대한 설명도 함께 제공된다.
+        # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
+        # 만족하고 잠자리에 든다.
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
