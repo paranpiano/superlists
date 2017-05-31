97404c27e053232ec7edcd63a535bfec1fce5231
parnapiano <paran_piano@hotmail.com>
2017-05-31 15:50:54 +0900

    added item model and migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from lists.models import Item
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -33,9 +34,22 @@ class HomePageTest(TestCase):
         )
         self.assertEqual(response.content.decode(), expected_html)
 
-#class SmokeTest(TestCase):
+class ItemModelTest(TestCase):
 
-#   def test_bad_maths(self):
-#       self.assertEqual(1+1 , 3)
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'first item'
+        first_item.save()
 
-# Create your tests here.
+        second_item = Item()
+        second_item.text = 'second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'first item')
+        self.assertEqual(second_saved_item.text, 'second item')
+
