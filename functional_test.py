ebe000feb04825b6f7a33fdc62d2ae56585e0dbd
parnapiano <paran_piano@hotmail.com>
2017-05-31 15:09:54 +0900

    refactoring - add check for row in list table but still there is assertion error.

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ class NewCisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self , row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
@@ -29,15 +34,22 @@ class NewCisitorTest(unittest.TestCase):
 
         # "공작깃털 사기"라고 텍스트 상자에 입력한다
         # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
-        inputbox.send_keys('Bying peacok feather')
+        inputbox.send_keys('Buying peacock feather')
         inputbox.send_keys(Keys.ENTER)
 
         import time
-        time.sleep(2)
-        table = self.browser.find_element_by_id('id_list_table')
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buying peacock feather')
 
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Bying peacok feather', [row.text for row in rows])
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
+        # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다 (에디스는 매우 체계적인 사람이다)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('making a net with peacock feather')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+        self.check_for_row_in_list_table('2: making a net with peacock feather')
+        self.check_for_row_in_list_table('1: Buying peacock feather')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "공작깃털을 사용해서 그물 만들기"라고 입력한다 (에디스는 매우 체계적인 사람이다)
