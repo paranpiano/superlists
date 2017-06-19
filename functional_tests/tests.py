3a0d96234f66f447a89f9db4a312ac5ec9e8695b
parnapiano <paran_piano@hotmail.com>
2017-06-19 14:36:42 +0900

    ConnectionResetError: [WinError 10054] An existing connection was forcibly closed by the remote host
    위에 이슈 해결

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -62,6 +62,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
          ## 새로운 브라우저 세션을 이용해서 에디스의 정보가
          ## 쿠키를 통해 유입되는 것을 방지한다
+         self.browser.refresh();
          self.browser.quit()
          self.browser = webdriver.Firefox()
 
