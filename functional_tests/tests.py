f58a9233169fa5b57703e62a5b60f0767b996819
parnapiano <paran_piano@hotmail.com>
2017-06-19 11:01:19 +0900

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -92,6 +92,19 @@ class NewVisitorTest(LiveServerTestCase):
 
          # 둘 다 만족하고 잠자리에 든다
 
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing\n')
+
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
 
